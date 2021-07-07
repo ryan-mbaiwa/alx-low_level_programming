34973ac5d835a114ee63941f4a8fc1bee240ad0d
ryan <ryanmbaiwa@gmail.com>
2021-07-07 13:52:35 -0700

    all files

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,7 +1,7 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
-int _putchar(char c);
+int _putchar(char);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
