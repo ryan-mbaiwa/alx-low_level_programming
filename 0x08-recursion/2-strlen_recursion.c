713ffe633b30a9c2b5fc51a6a349ab6bc2d94558
ryan <ryanmbaiwa@gmail.com>
2021-07-07 13:58:11 -0700

    betty style adjustment

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
-/** 
+/**
  * _strlen_recursion - returns the length of a string.
  * @s: string
- * Return: the length of string.
+ * Return: the length of a string.
  */
 int _strlen_recursion(char *s)
 {
