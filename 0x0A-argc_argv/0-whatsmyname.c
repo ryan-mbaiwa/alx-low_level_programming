53ac0cd4a6d929249e306f7b0e6d9fff112de83c
ryan <ryanmbaiwa@gmail.com>
2021-07-13 00:33:10 -0700

    file that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the name of the program
+ * @argc: the legnth of arguments
+ * @argv: the array holding the argument
+ * Return: always zero
+ */
+
+int main(int argc __attribute_((unused))_, char *argv[])
+{
+printf("%s\n", argv[0]);
+
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,4 @@
+Writing arguments to the main function
+using argc and argv
+The two main prototypes to main and when to use them
+using arguments passed to variables
