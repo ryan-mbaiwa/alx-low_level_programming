0a822845d10d1d9b3ce9257dd7e8e02498a1d390
ryan <ryanmbaiwa@gmail.com>
2021-07-13 01:03:21 -0700

    errorfixed on betty style

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to it
+ * @argc: The number of aguments.
+ * @argv: The array to the arguments in c
+ * Return: 0 - success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc++);
+return (0);
+}
