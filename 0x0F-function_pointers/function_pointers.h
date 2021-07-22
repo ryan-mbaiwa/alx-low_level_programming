5c16da63453a6b900c05a0caa066248ac3316196
ryan <ryanmbaiwa@gmail.com>
2021-07-22 12:09:04 -0700

    error fixed on header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef _FUNCTION_POINTERS_
-#define _FUNCTION_POINTERS_
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
 #include <stdio.h>
 
 void print_name(char *name, void (*f)(char *));
