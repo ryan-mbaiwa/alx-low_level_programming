2ddbfa10267b76cc7b08c19536a72da2df6c2f1f
ryan <ryanmbaiwa@gmail.com>
2021-07-22 03:06:18 -0700

    0

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,19 +1,23 @@
 #include "function_pointers.h"
-
 /**
- * array_iterator - executes a function given as a
- * parameter on each element of an array.
- * @array: input integer array.
- * @size: size of the array.
- * @action: pointer to the function.
+ * array_iterator - function that executes a function given as a parameter
+ * on each element of an array
+ * @array: point to array of integers
+ * @size: lenght of array input
+ * @action: pointer to function that prints each element of an array
  *
- * Return: no return.
+ * Return: Void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
+	size_t i = 0;
 
-	if (array && action)
-		for (i = 0; i < size; i++)
-			action(array[i]);
+	if (action && array)
+	{
+		while (i < size)
+		{
+			(*action)(array[i]);
+			i++;
+		}
+	}
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
 #ifndef _FUNCTION_POINTERS_
 #define _FUNCTION_POINTERS_
+#include <stdio.h>
 
-int _putchar(int c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int);
