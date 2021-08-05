cdf9810a4166bc4235d1b42153fa1e2a62559947
ryan <ryanmbaiwa@gmail.com>
2021-08-05 10:23:38 -0700

    all files

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - function that converts a char binary to int
+ * @b: binary in char
+ * Return: number int; 0 if b is not binary so b is NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec = 0, i = 0, res = 0, len;
+
+	if (b == NULL || b[0] == '\0')
+		return (0);
+	/*condition to check if it is binary or not*/
+	for (len = 0; b[len] != '\0'; len++)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+	}
+	len = len - 1;
+	/*ehil << (len - i)= mmult 2^2 */
+	while (i <= len)
+	{
+		dec = (b[i] - '0') << (len - i);
+		res = dec + res;
+		i++;
+	}
+	return (res);
+}
